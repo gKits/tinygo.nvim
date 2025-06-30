6a67b67208f37372c7c4f289321da417bd8b7949
gkits <gkitsikoudis@gmx.de>
2025-06-30 12:34:16 +0200

    feat: add tinygo flash and monitor commands

--- lua/tinygo/init.lua
+++ lua/tinygo/init.lua
@@ -1,5 +1,10 @@
 local M = {
     config_file = ".tinygo.json",
+
+    floating = {
+        buf = -1,
+        win = -1,
+    },
 }
 
 function M.setup(opts)
@@ -36,6 +41,8 @@ function M.setup(opts)
 	vim.api.nvim_create_user_command("TinyGoSetTarget", M.setTarget, {nargs = 1, complete = M.targetOptions})
 	vim.api.nvim_create_user_command("TinyGoTargets", M.printTargets, {nargs = 0})
 	vim.api.nvim_create_user_command("TinyGoEnv", M.printEnv, {nargs = 0})
+	vim.api.nvim_create_user_command("TinyGoFlash", M.flash, {nargs = "?", complete = M.targetOptions})
+	vim.api.nvim_create_user_command("TinyGoMonitor", M.toggleMonitor, {nargs = "*"})
 
     vim.api.nvim_create_autocmd({"LspAttach"}, {once=true, pattern="*.go", callback=M.applyConfigFile})
     vim.api.nvim_create_autocmd({"BufWritePost"}, {pattern=M.config_file, callback=M.applyConfigFile})
@@ -108,6 +115,26 @@ function M.setTarget(opts)
 	 })
 end
 
+function M.flash(opts)
+    local target = opts.fargs[1]
+    vim.print(target)
+    if not target then
+        target = M["currentTarget"]
+    end
+
+    if target == "original" then
+        vim.print("cannot flash: no target set")
+        return
+    end
+
+    vim.system({"tinygo", "flash", "-target", target, "."}, {
+        text = true,
+        stdout = function (_, data) if data then vim.print(data) end end,
+        stderr = function (_, data) if data then vim.print(data) end end,
+    }):wait()
+end
+
+
 function M.printTargets()
 	local ok, targets = pcall(vim.fn.system, "tinygo targets")
 	if not ok then
@@ -148,6 +175,78 @@ function M.applyConfigFile()
     if target then
         vim.cmd.TinyGoSetTarget(target)
     end
+end
+
+local function createFloatingWindow(opts)
+    opts = opts or {}
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.8)
+
+    local col = math.floor((vim.o.columns - width) / 2)
+    local row = math.floor((vim.o.lines - height) / 2)
+
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    if opts.title then
+        local padding = string.rep(" ", (width - #opts.title) / 2)
+        local title = padding .. opts.title
+        vim.api.nvim_buf_set_lines(buf, 0, 1, false, {title})
+    end
+
+    vim.bo[buf].modifiable = false
+    vim.bo[buf].modified = false
+
+    local win_config = {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = col,
+        row = row,
+        style = "minimal",
+        border = "rounded",
+    }
+    local win = vim.api.nvim_open_win(buf, true, win_config)
+    return { buf = buf, win = win }
+end
+
+function M.toggleMonitor(opts)
+    M.floating = createFloatingWindow({title = "Monitor"})
+
+    if M.floating.job then
+        M.floating.job:kill(1)
+    end
+
+    vim.api.nvim_create_autocmd({"BufLeave"}, {once=true, buffer=M.floating.buf, callback=function ()
+        if M.floating.job then
+            M.floating.job:kill(1)
+        end
+        M.floating = { buf = -1, win = -1 }
+    end})
+
+    local args = ""
+    if opts.args then
+        args = opts.args
+    end
+
+    M.floating.job = vim.system( {"tinygo", "monitor", args}, {
+        text = true,
+        stdout = M.writeToFloatingWindow,
+        stderr = M.writeToFloatingWindow,
+    })
+end
+
+function M.writeToFloatingWindow(_, data)
+    if not data or M.floating.buf == -1 or M.floating.win == -1 then
+        return
+    end
+    vim.schedule(function ()
+        local escaped = vim.fn.split(data, "\n", false)
+        vim.bo[M.floating.buf].modifiable = true
+        vim.api.nvim_buf_set_lines(M.floating.buf, -1, -1, true, escaped)
+        vim.api.nvim_win_set_cursor(M.floating.win, {vim.api.nvim_buf_line_count(M.floating.buf), 0})
+        vim.bo[M.floating.buf].modifiable = false
+        vim.bo[M.floating.buf].modified = false
+    end)
 end
 
 return M
