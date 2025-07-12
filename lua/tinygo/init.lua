d4b676da7e1b3d302149de3d7fa2b262f08df658
gkits <gkitsikoudis@gmx.de>
2025-07-13 00:52:35 +0200

    style: use tabs instead of spaces

--- lua/tinygo/init.lua
+++ lua/tinygo/init.lua
@@ -1,11 +1,11 @@
 local M = {
-    config_file = ".tinygo.json",
-    tinygo = "tinygo",
+	config_file = ".tinygo.json",
+	tinygo = "tinygo",
 
-    floating = {
-        buf = -1,
-        win = -1,
-    },
+	floating = {
+		buf = -1,
+		win = -1,
+	},
 }
 
 function M.setup(opts)
@@ -54,21 +54,12 @@ function M.setup(opts)
 end
 
 function M.loadOptions(opts)
-    if opts["config_file"] then
-        M.config_file = opts["config_file"]
-    end
-    if opts["cmd"] then
-        M.tinygo = opts["cmd"]
-    end
-end
-
-function M.loadOptions(opts)
-    if opts["config_file"] then
-        M.config_file = opts["config_file"]
-    end
-    if opts["cmd"] then
-        M.tinygo = opts["cmd"]
-    end
+	if opts["config_file"] then
+		M.config_file = opts["config_file"]
+	end
+	if opts["cmd"] then
+		M.tinygo = opts["cmd"]
+	end
 end
 
 -- As seen on https://neovim.io/doc/user/api.html#nvim_create_user_command(), autocompletions written in
@@ -139,22 +130,22 @@ function M.setTarget(opts)
 end
 
 function M.flash(opts)
-    local target = opts.fargs[1]
-    vim.print(target)
-    if not target then
-        target = M["currentTarget"]
-    end
+	local target = opts.fargs[1]
+	vim.print(target)
+	if not target then
+		target = M["currentTarget"]
+	end
 
-    if target == "original" then
-        vim.print("cannot flash: no target set")
-        return
-    end
+	if target == "original" then
+		vim.print("cannot flash: no target set")
+		return
+	end
 
-    vim.system({M.tinygo, "flash", "-target", target, "."}, {
-        text = true,
-        stdout = function (_, data) if data then vim.print(data) end end,
-        stderr = function (_, data) if data then vim.print(data) end end,
-    }):wait()
+	vim.system({M.tinygo, "flash", "-target", target, "."}, {
+		text = true,
+		stdout = function (_, data) if data then vim.print(data) end end,
+		stderr = function (_, data) if data then vim.print(data) end end,
+	}):wait()
 end
 
 
@@ -201,147 +192,147 @@ function M.applyConfigFile()
 end
 
 local function createFloatingWindow(opts)
-    opts = opts or {}
-    local width = math.floor(vim.o.columns * 0.8)
-    local height = math.floor(vim.o.lines * 0.8)
+	opts = opts or {}
+	local width = math.floor(vim.o.columns * 0.8)
+	local height = math.floor(vim.o.lines * 0.8)
 
-    local col = math.floor((vim.o.columns - width) / 2)
-    local row = math.floor((vim.o.lines - height) / 2)
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - height) / 2)
 
-    local buf = vim.api.nvim_create_buf(false, true)
+	local buf = vim.api.nvim_create_buf(false, true)
 
-    if opts.title then
-        local padding = string.rep(" ", (width - #opts.title) / 2)
-        local title = padding .. opts.title
-        vim.api.nvim_buf_set_lines(buf, 0, 1, false, {title})
-    end
+	if opts.title then
+		local padding = string.rep(" ", (width - #opts.title) / 2)
+		local title = padding .. opts.title
+		vim.api.nvim_buf_set_lines(buf, 0, 1, false, {title})
+	end
 
-    vim.bo[buf].modifiable = false
-    vim.bo[buf].modified = false
+	vim.bo[buf].modifiable = false
+	vim.bo[buf].modified = false
 
-    local win_config = {
-        relative = "editor",
-        width = width,
-        height = height,
-        col = col,
-        row = row,
-        style = "minimal",
-        border = "rounded",
-    }
-    local win = vim.api.nvim_open_win(buf, true, win_config)
-    return { buf = buf, win = win }
+	local win_config = {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+		border = "rounded",
+	}
+	local win = vim.api.nvim_open_win(buf, true, win_config)
+	return { buf = buf, win = win }
 end
 
 function M.toggleMonitor(opts)
-    M.floating = createFloatingWindow({title = "Monitor"})
+	M.floating = createFloatingWindow({title = "Monitor"})
 
-    if M.floating.job then
-        M.floating.job:kill(1)
-    end
+	if M.floating.job then
+		M.floating.job:kill(1)
+	end
 
-    vim.api.nvim_create_autocmd({"BufLeave"}, {once=true, buffer=M.floating.buf, callback=function ()
-        if M.floating.job then
-            M.floating.job:kill(1)
-        end
-        M.floating = { buf = -1, win = -1 }
-    end})
+	vim.api.nvim_create_autocmd({"BufLeave"}, {once=true, buffer=M.floating.buf, callback=function ()
+		if M.floating.job then
+			M.floating.job:kill(1)
+		end
+		M.floating = { buf = -1, win = -1 }
+	end})
 
-    local args = ""
-    if opts.args then
-        args = opts.args
-    end
+	local args = ""
+	if opts.args then
+		args = opts.args
+	end
 
-    M.floating.job = vim.system( {M.tinygo, "monitor", args}, {
-        text = true,
-        stdout = M.writeToFloatingWindow,
-        stderr = M.writeToFloatingWindow,
-    })
+	M.floating.job = vim.system( {M.tinygo, "monitor", args}, {
+		text = true,
+		stdout = M.writeToFloatingWindow,
+		stderr = M.writeToFloatingWindow,
+	})
 end
 
 function M.writeToFloatingWindow(_, data)
-    if not data or M.floating.buf == -1 or M.floating.win == -1 then
-        return
-    end
-    vim.schedule(function ()
-        local escaped = vim.fn.split(data, "\n", false)
-        vim.bo[M.floating.buf].modifiable = true
-        vim.api.nvim_buf_set_lines(M.floating.buf, -1, -1, true, escaped)
-        vim.api.nvim_win_set_cursor(M.floating.win, {vim.api.nvim_buf_line_count(M.floating.buf), 0})
-        vim.bo[M.floating.buf].modifiable = false
-        vim.bo[M.floating.buf].modified = false
-    end)
+	if not data or M.floating.buf == -1 or M.floating.win == -1 then
+		return
+	end
+	vim.schedule(function ()
+		local escaped = vim.fn.split(data, "\n", false)
+		vim.bo[M.floating.buf].modifiable = true
+		vim.api.nvim_buf_set_lines(M.floating.buf, -1, -1, true, escaped)
+		vim.api.nvim_win_set_cursor(M.floating.win, {vim.api.nvim_buf_line_count(M.floating.buf), 0})
+		vim.bo[M.floating.buf].modifiable = false
+		vim.bo[M.floating.buf].modified = false
+	end)
 end
 
 local function createFloatingWindow(opts)
-    opts = opts or {}
-    local width = math.floor(vim.o.columns * 0.8)
-    local height = math.floor(vim.o.lines * 0.8)
+	opts = opts or {}
+	local width = math.floor(vim.o.columns * 0.8)
+	local height = math.floor(vim.o.lines * 0.8)
 
-    local col = math.floor((vim.o.columns - width) / 2)
-    local row = math.floor((vim.o.lines - height) / 2)
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - height) / 2)
 
-    local buf = vim.api.nvim_create_buf(false, true)
+	local buf = vim.api.nvim_create_buf(false, true)
 
-    if opts.title then
-        local padding = string.rep(" ", (width - #opts.title) / 2)
-        local title = padding .. opts.title
-        vim.api.nvim_buf_set_lines(buf, 0, 1, false, {title})
-    end
+	if opts.title then
+		local padding = string.rep(" ", (width - #opts.title) / 2)
+		local title = padding .. opts.title
+		vim.api.nvim_buf_set_lines(buf, 0, 1, false, {title})
+	end
 
-    vim.bo[buf].modifiable = false
-    vim.bo[buf].modified = false
+	vim.bo[buf].modifiable = false
+	vim.bo[buf].modified = false
 
-    local win_config = {
-        relative = "editor",
-        width = width,
-        height = height,
-        col = col,
-        row = row,
-        style = "minimal",
-        border = "rounded",
-    }
-    local win = vim.api.nvim_open_win(buf, true, win_config)
-    return { buf = buf, win = win }
+	local win_config = {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+		border = "rounded",
+	}
+	local win = vim.api.nvim_open_win(buf, true, win_config)
+	return { buf = buf, win = win }
 end
 
 function M.toggleMonitor(opts)
-    M.floating = createFloatingWindow({title = "Monitor"})
+	M.floating = createFloatingWindow({title = "Monitor"})
 
-    if M.floating.job then
-        M.floating.job:kill(1)
-    end
+	if M.floating.job then
+		M.floating.job:kill(1)
+	end
 
-    vim.api.nvim_create_autocmd({"BufLeave"}, {once=true, buffer=M.floating.buf, callback=function ()
-        if M.floating.job then
-            M.floating.job:kill(1)
-        end
-        M.floating = { buf = -1, win = -1 }
-    end})
+	vim.api.nvim_create_autocmd({"BufLeave"}, {once=true, buffer=M.floating.buf, callback=function ()
+		if M.floating.job then
+			M.floating.job:kill(1)
+		end
+		M.floating = { buf = -1, win = -1 }
+	end})
 
-    local args = ""
-    if opts.args then
-        args = opts.args
-    end
+	local args = ""
+	if opts.args then
+		args = opts.args
+	end
 
-    M.floating.job = vim.system( {M.tinygo, "monitor", args}, {
-        text = true,
-        stdout = M.writeToFloatingWindow,
-        stderr = M.writeToFloatingWindow,
-    })
+	M.floating.job = vim.system( {M.tinygo, "monitor", args}, {
+		text = true,
+		stdout = M.writeToFloatingWindow,
+		stderr = M.writeToFloatingWindow,
+	})
 end
 
 function M.writeToFloatingWindow(_, data)
-    if not data or M.floating.buf == -1 or M.floating.win == -1 then
-        return
-    end
-    vim.schedule(function ()
-        local escaped = vim.fn.split(data, "\n", false)
-        vim.bo[M.floating.buf].modifiable = true
-        vim.api.nvim_buf_set_lines(M.floating.buf, -1, -1, true, escaped)
-        vim.api.nvim_win_set_cursor(M.floating.win, {vim.api.nvim_buf_line_count(M.floating.buf), 0})
-        vim.bo[M.floating.buf].modifiable = false
-        vim.bo[M.floating.buf].modified = false
-    end)
+	if not data or M.floating.buf == -1 or M.floating.win == -1 then
+		return
+	end
+	vim.schedule(function ()
+		local escaped = vim.fn.split(data, "\n", false)
+		vim.bo[M.floating.buf].modifiable = true
+		vim.api.nvim_buf_set_lines(M.floating.buf, -1, -1, true, escaped)
+		vim.api.nvim_win_set_cursor(M.floating.win, {vim.api.nvim_buf_line_count(M.floating.buf), 0})
+		vim.bo[M.floating.buf].modifiable = false
+		vim.bo[M.floating.buf].modified = false
+	end)
 end
 
 return M
